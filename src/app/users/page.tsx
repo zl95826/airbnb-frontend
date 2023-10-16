18b81f4ed55d7d19c58085663115d3efaece5198
zl95826 <zl95826@hotmail.com>
2023-10-15 21:43:54 -0700

    cache: keep data fresh for a certain period of time

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -3,10 +3,21 @@ interface User {
   id: number;
   name: string;
 }
+//------------------one option
+//keep data fresh for a certain period of time
 const UsersPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    cache: "no-store",
+    next: { revalidate: 10 },
+    //In next object, we can specify configuration parameters
+    //that are specific to nexjs
+    //This means next.js is going to run a background job,
+    //and get fresh data from the backend every 10 seconds.
   });
+  //-----------------another option
+  //   const res = await fetch("https://jsonplaceholder.typicode.com/users",
+  //   {
+  //     cache: "no-store",
+  //   });
   //{cache:"no-store"}  disable caching that's one option and this is useful
   //if you have data that changes frequently.
   // In those situations we always want to show fresh data to our users.
